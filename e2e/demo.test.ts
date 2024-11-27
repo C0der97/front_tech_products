57f5c3d0210fe0e9e393b6c9fe2799c3be6ddcce
c0der97 <blae1997@gmail.com>
2024-11-26 19:07:14 -0500

    mejoras

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,6 +1,6 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+test('example test', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle(/Example/);
 });
